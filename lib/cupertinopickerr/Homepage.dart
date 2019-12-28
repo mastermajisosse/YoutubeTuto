8bc5638f5f259a8616d285a4b2e02345fb1298bd
otmane <mastermajidosse@gmail.com>
2019-12-28 09:04:10 +0100

    new

---
+++ lib/cupertinopickerr/Homepage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selected = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("CupertinoPicker"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          height: 250,
+          alignment: Alignment.center,
+          width: double.infinity,
+          child: CupertinoPicker(
+            backgroundColor: Colors.white,
+            itemExtent: 50,
+            onSelectedItemChanged: (int i) {
+              setState(() {
+                selected = i;
+              });
+            },
+            looping: true,
+            children: <Widget>[
+              Center(
+                child: Text(
+                  "data",
+                  style: TextStyle(
+                      color: selected == 0 ? Colors.blue : Colors.green),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "data2",
+                  style: TextStyle(
+                      color: selected == 1 ? Colors.blue : Colors.green),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "data3",
+                  style: TextStyle(
+                      color: selected == 2 ? Colors.blue : Colors.green),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
